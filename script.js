853a44642f06efb9b03f892c7998bd6e281eafde
tore <aldemirtore@gmail.com>
2023-01-05 12:49:06 +0300

    second commit with ui

--- script.js
+++ script.js
@@ -1,90 +1,48 @@
-let playerScore = 0;
-let computerScore = 0;
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
 
-const options = ["rock", "paper", "scissors"];
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let cScore=0;
+let pScore=0;
 
-function getComputerChoice(){
-   const choice = options[Math.floor(Math.random() * options.length)]
-   return choice;
-}
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText .textContent = `Player: ${player} ===== Score: ${pScore}`;
+    computerText.textContent = `Computer ${computer} ===== Score: ${cScore}`;
+    resultText.textContent = checkWinner();
+}))
 
-function checkWinner(playerSelection, computerSelection){
-    if(playerSelection == computerSelection){
-        return "Tie";
-    }
+function computerTurn(){
+    const randNum = Math.floor(Math.random() * 3) + 1;
 
-    else if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "rock")||
-        (playerSelection == "scissors" && computerSelection == "paper")
-    ){
-        return "Player"
-    }
-    else {
-        return "Computer"
-    }
-}
-
-function playRound(playerSelection, computerSelection){
-    const result = checkWinner (playerSelection, computerSelection);
-    if(result == "Tie"){
-        return "Its a Tie!"
-    }
-    else if (result == "Player"){
-        return `You Win! ${playerSelection} beats ${computerSelection}`
-    }
-
-    else{
-        return `You Loose ${computerSelection} beats ${playerSelection}`
+    switch(randNum){
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
     }
 }
 
-function getPlayerChoice(){
-    let validatedInput = false;
-    while(validatedInput == false){
-        const choice = prompt("Rock Paper Scissors");
-        if(choice == null){
-            continue;
-        }
-        const choiceInLower = choice.toLowerCase();
-
-        if(options.includes(choiceInLower)){
-            validatedInput = true;
-            return choiceInLower;
-        }
+function checkWinner(){
+    if(player == computer){
+        return "Draw!";
+    }
+    else if(computer == "ROCK"){
+        return(player == "PAPER") ? "You Win! " + pScore++ : "You Loose! " + cScore++;
+    }
+    else if (computer == "PAPER"){
+        return(player == "SCISSORS") ? "You WIn! " + pScore++: "You Loose " + cScore++;
+    }
+    else if(computer == "SCISSORS"){
+        return(player == "ROCK" ? "You Win! " + pScore++: "You Loose " + cScore++);
     }
 }
-
-function game(){
-
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    console.log(`torererre`)
-    for (let i= 0; i<5; i++){
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-
-        console.log("-------------------------")
-        if(checkWinner(playerSelection, computerSelection) == "Player"){
-            scorePlayer++;
-        }
-        else if(checkWinner(playerSelection, computerSelection) == "Computer"){
-            scoreComputer++;
-        }
-    }
-
-    console.log(`GAME OVER!`);
-    if(scorePlayer > scoreComputer){
-        console.log("Player is the winner!")
-    }
-    else if ( scorePlayer < scoreComputer)
-    {
-        console.log("Computer is the winner!")
-    }
-    else{
-        console.log("TIEEEE!!")
-    }
-}
-
-game()
